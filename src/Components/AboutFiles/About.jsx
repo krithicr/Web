d3885b7cfd322698446dc9d2d3062a72c80ed20a
krithicr <krithicraj.1902101@srec.ac.in>
2024-03-30 00:13:01 +0530

    npm reinstalled

--- src/Components/AboutFiles/About.jsx
+++ src/Components/AboutFiles/About.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-
-// import HeroBgAnimation from "../HeroBgAnimation";
 import {
   HeroContainer,
-  HeroBg,
   HeroLeftContainer,
   HeroRightContainer,
   HeroInnerContainer,
